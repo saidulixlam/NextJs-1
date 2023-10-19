76a6ee65944d485dd1e098159ff20ad8d2ebf526
saidulixlam <saidulixlam@gmail.com>
2023-10-19 13:36:41 +0530

    Dynamic routing

--- pages/aboutus/developer.js
+++
@@ -1,4 +0,0 @@
-function Developer(){
-    return <h1>The Dev page of about</h1>
-}
-export default Developer;
---
+++ pages/aboutus/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+function Developer(){
+    return (
+    <Fragment>
+        <h1>This is the developer page </h1>
+        
+    </Fragment>
+    )
+}
+export default Developer;
