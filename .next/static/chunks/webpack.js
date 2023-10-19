fbf55f8f59b8355dd63dacd904754c3fcfb1cbcd
saidulixlam <saidulixlam@gmail.com>
2023-10-19 12:05:48 +0530

    nested routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6048623012edac11"; }
+/******/ 		__webpack_require__.h = function() { return "5ac97cc2aa4c4f95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/aboutus/developer.js
@@ -0,0 +1,4 @@
+function Developer(){
+    return <h1>The Dev page of about</h1>
+}
+export default Developer;
