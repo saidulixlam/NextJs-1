621d16f213447b3a73661cba7766daf9cac15a25
saidulixlam <saidulixlam@gmail.com>
2023-10-19 20:51:52 +0530

    Dynamic routing

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,11 +1,32 @@
+import Link from "next/link";
 import { Fragment } from "react";
 
-function Developer(){
+function Developer() {
     return (
-    <Fragment>
-        <h1>This is the developer page </h1>
-        
-    </Fragment>
+        <Fragment>
+            <h1>develpoers world welcomes you </h1>
+            <h4>
+                Our fine developers ....
+            </h4>
+            <ul>
+                <li>
+                    <Link href='/aboutus/1'>
+                        Yash
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/aboutus/2'>
+                        Vaibhab
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/aboutus/3'>
+                        Suresh
+                    </Link>
+                </li>
+            </ul>
+
+        </Fragment>
     )
 }
 export default Developer;
